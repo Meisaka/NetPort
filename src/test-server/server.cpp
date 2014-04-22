6401b4906803e0ddf31c494dc9cf86722cf0e714
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2014-04-22 12:28:37 +0900

    moved items in the test-server for RAII

--- src/test-server/server.cpp
+++ src/test-server/server.cpp
@@ -5,13 +5,14 @@
 
 int main(int argc, char *argv[])
 {
-	network::MEISystem sys;
-
 	network::Initialize();
 
-	sys.Init(network::ADDRTYPE::NETA_IPv4);
+	{
+		network::MEISystem sys;
+		sys.Init(network::ADDRTYPE::NETA_IPv4);
+		std::this_thread::sleep_for(std::chrono::seconds(10));
+	}
 
-	std::this_thread::sleep_for(std::chrono::seconds(20));
 	return 0;
 }
 
